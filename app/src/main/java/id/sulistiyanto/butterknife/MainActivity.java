214c4d02636f1f1e488ceee2cc97fb5319b4327f
Sulistiyanto <sulistiyanto404@gmail.com>
2016-12-01 06:28:28 +0700

    bind

--- app/src/main/java/id/sulistiyanto/butterknife/MainActivity.java
+++ app/src/main/java/id/sulistiyanto/butterknife/MainActivity.java
@@ -2,12 +2,28 @@ package id.sulistiyanto.butterknife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editText1) EditText editText1;
+    @BindView(R.id.editText2) EditText editText2;
+    @BindView(R.id.txtHasil) TextView txtHasil;
+
+    @OnClick(R.id.button) void jumlah() {
+        Toast.makeText(this, "sfsdf", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
